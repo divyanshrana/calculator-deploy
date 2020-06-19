9a1bc3e9475e5ab7eb04250d6255d6eb67e2c3da
divyanshrana <divyansh.rana12@gmail.com>
2020-06-19 19:35:50 +0530

    Updates

---
+++ precache-manifest.d8607c4e3c9b6de07e9a4f7734e0086d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04bd211fcc051e7bfcb11146ae1aa95",
+    "url": "/calculator-deploy/index.html"
+  },
+  {
+    "revision": "ae423fdcf1aa1277947c",
+    "url": "/calculator-deploy/static/css/main.a4191227.chunk.css"
+  },
+  {
+    "revision": "d4e99a68a5608ed86c87",
+    "url": "/calculator-deploy/static/js/2.f462ee8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-deploy/static/js/2.f462ee8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae423fdcf1aa1277947c",
+    "url": "/calculator-deploy/static/js/main.7d18b369.chunk.js"
+  },
+  {
+    "revision": "f0bb3249c6103b3910a4",
+    "url": "/calculator-deploy/static/js/runtime-main.d7c1e485.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
